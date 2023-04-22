30e99bb3bfb738b0fefeb4b038a540995d42f3ec
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2023-04-22 13:48:10 +0200

    feat: add properties for car

---
+++ AutoRepairShop.Logic/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoRepairShop.Logic.Models
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public DateTime ModelYear { get; set; }
+        public DateTime LastInspectionDate { get; set; }
+    }
+}
