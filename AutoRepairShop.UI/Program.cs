57fb50df3a0aac482cc95e81d2959e425a7a38df
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2023-04-22 11:20:04 +0200

    feat: add try/catch; add while loop and exit btn; add folders for logic

--- AutoRepairShop.UI/Program.cs
+++ AutoRepairShop.UI/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    try
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Fejl: {ex.Message}");
+    }
+    Console.WriteLine("Tryk 'q' for at afslutte eller en anden vilkårlig tast for at genstarte programmet.");
+    if (Console.ReadKey().Key == ConsoleKey.Q) break;
+    Console.Clear();
+}
