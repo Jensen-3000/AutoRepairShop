2b1f9dba064116d23b7cc85e587efc1ae0b2168d
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2023-04-22 12:37:12 +0200

    feat: add inputs for car brand, model, year, last inspection; add exception handling; add parse handling

--- AutoRepairShop.UI/Program.cs
+++ AutoRepairShop.UI/Program.cs
@@ -1,8 +1,21 @@
-﻿while (true)
+﻿using System.Globalization;
+
+while (true)
 {
+    int dateMinValue = 1900;
+    int dateMaxValue = DateTime.Now.Year + 1;
+
     try
     {
-        Console.WriteLine("Hello, World!");
+        string brandInput = GetInputFromUser("Indtast bilmærke: ");
+
+        string modelInput = GetInputFromUser("Indtast bilmodel: ");
+
+        string yearInput = GetInputFromUser("Indtast bilens årgang: ");
+        DateTime modelYear = GetCarModelYear(yearInput, dateMinValue, dateMaxValue);
+
+        string inspectionDateInput = GetInputFromUser("Indtast bilens sidste syns dato (dd-MM-yyyy) \nHvis bilen ikke har været synet før tast 0000: ");
+        DateTime lastInspectionDate = GetLastInspectionDate(inspectionDateInput, dateMinValue, dateMaxValue);
     }
     catch (Exception ex)
     {
@@ -11,4 +24,48 @@
     Console.WriteLine("Tryk 'q' for at afslutte eller en anden vilkårlig tast for at genstarte programmet.");
     if (Console.ReadKey().Key == ConsoleKey.Q) break;
     Console.Clear();
+}
+
+// ToDo: Move methods into classes and refactor and DRY DateTime methods
+string GetInputFromUser(string prompt)
+{
+    Console.Write(prompt);
+    return Console.ReadLine().ToLower().Trim();
+}
+
+DateTime GetCarModelYear(string input, int minYear, int maxYear)
+{
+    DateTime modelYear;
+
+    if (!DateTime.TryParseExact(input, "yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out modelYear))
+    {
+        throw new FormatException("Forkert format. Skal være yyyy. fx. 1999");
+    }
+
+    if (modelYear.Year < minYear || modelYear.Year > maxYear)
+    {
+        throw new ArgumentOutOfRangeException("modelYear", $"Årgang skal være mellem {minYear} og {maxYear}.");
+    }
+    return modelYear;
+}
+
+DateTime GetLastInspectionDate(string input, int minYear, int maxYear)
+{
+    DateTime lastInspectionDate;
+
+    if (input == "0000")
+    {
+        lastInspectionDate = DateTime.MinValue;
+    }
+
+    if (!DateTime.TryParseExact(input, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out lastInspectionDate))
+    {
+        throw new FormatException("Forkert format. Skal være dd-MM-yyyy. fx. 01-12-1999");
+    }
+
+    if (lastInspectionDate.Year < minYear || lastInspectionDate.Year > maxYear)
+    {
+        throw new ArgumentOutOfRangeException("lastInspectionDate", $"Sidste syns dato skal være mellem {minYear} og {maxYear}.");
+    }
+    return lastInspectionDate;
 }
