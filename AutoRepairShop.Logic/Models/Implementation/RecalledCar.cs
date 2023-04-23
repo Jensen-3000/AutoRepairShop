e3c93d0f25b27f06b1996ad10f7ab10885fd32a1
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2023-04-23 11:06:14 +0200

    refactor: moved car array into its own class

--- AutoRepairShop.Logic/Models/Implementation/RecalledCar.cs
+++ AutoRepairShop.Logic/Models/Implementation/RecalledCar.cs
@@ -11,25 +11,4 @@ namespace AutoRepairShop.Logic.Models.Implementation
         public DateTime RecallThresholdDate { get; set; }
         public string FactoryDefect { get; set; }
     }
-
-    public class RecalledCars
-    {
-        public RecalledCar[] recalledCars = new RecalledCar[]
-        {
-            new RecalledCar
-            {
-                Brand = "Fiat",
-                Model = "Punto",
-                RecallThresholdDate = new DateTime(2010,01,01),
-                FactoryDefect = "Udstødning"
-            },
-            new RecalledCar
-            {
-                Brand = "Alfa Romeo",
-                Model = "Giulia",
-                RecallThresholdDate = new DateTime(2019,07,01),
-                FactoryDefect = "Styretøjet"
-            }
-        };
-    }
 }
---
+++ AutoRepairShop.Logic/Models/Implementation/RecalledCars.cs
@@ -0,0 +1,25 @@
+﻿using AutoRepairShop.Logic.Models.Interfaces;
+
+namespace AutoRepairShop.Logic.Models.Implementation
+{
+    public class RecalledCars : IRecalledCars
+    {
+        public RecalledCar[] Cars { get; } = new RecalledCar[]
+        {
+            new RecalledCar
+            {
+                Brand = "Fiat",
+                Model = "Punto",
+                RecallThresholdDate = new DateTime(2010,01,01),
+                FactoryDefect = "Udstødning"
+            },
+            new RecalledCar
+            {
+                Brand = "Alfa Romeo",
+                Model = "Giulia",
+                RecallThresholdDate = new DateTime(2019,07,01),
+                FactoryDefect = "Styretøjet"
+            }
+        };
+    }
+}
